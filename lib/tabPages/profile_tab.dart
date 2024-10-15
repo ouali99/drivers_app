603ceeb9f2282979a18d22c4f79ad9f9fd14f2b1
ouali99 <oualiob2@gmail.com>
2024-10-14 22:02:12 -0400

    added logout button in profil ton desconnect

--- lib/tabPages/profile_tab.dart
+++ lib/tabPages/profile_tab.dart
@@ -1,4 +1,7 @@
+import 'package:drivers_app/global/global.dart';
+import 'package:drivers_app/splashScreen/splash_screen.dart';
 import 'package:flutter/material.dart';
+
 
 class ProfileTabPage extends StatefulWidget {
   const ProfileTabPage({Key? key}) : super(key: key);
@@ -8,16 +11,21 @@ class ProfileTabPage extends StatefulWidget {
 }
 
 
-class _ProfileTabPageState extends State<ProfileTabPage>
-{
+
+class _ProfileTabPageState extends State<ProfileTabPage> {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Center(
-      child: Text(
-          "Profile"
+      child: ElevatedButton(
+        child: const Text(
+          "Sign Out",
+        ),
+        onPressed: ()
+        {
+          fAuth.signOut();
+          Navigator.push(context, MaterialPageRoute(builder: (c)=> const MySplashScreen()));
+        },
       ),
     );
   }
-
 }
