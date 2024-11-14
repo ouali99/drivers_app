dd12d134f58db0c3c81b54e9904eb60970db459c
Jackfm54 <jackgomez72@gmail.com>
2024-11-14 11:59:05 -0500

    add biometric condiction

--- lib/authentification/login_screen.dart
+++ lib/authentification/login_screen.dart
@@ -6,85 +6,89 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:local_auth/local_auth.dart';
 
-
-class LoginScreen extends StatefulWidget
-{
-
+class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-
-
-
-class _LoginScreenState extends State<LoginScreen>
-{
+class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
+  final LocalAuthentication auth = LocalAuthentication();
 
+  Future<void> authenticateBiometric() async {
+    bool authenticated = false;
+    try {
+      authenticated = await auth.authenticate(
+        localizedReason: 'Veuillez vous authentifier pour vous connecter',
+        options: const AuthenticationOptions(
+          useErrorDialogs: true,
+          stickyAuth: true,
+        ),
+      );
+      if (authenticated) {
+        // Si authentification réussie, valider le formulaire et procéder à la connexion
+        validateForm();
+      } else {
+        Fluttertoast.showToast(msg: "Authentification biométrique échouée.");
+      }
+    } catch (e) {
+      Fluttertoast.showToast(msg: "Erreur d'authentification biométrique: $e");
+    }
+  }
 
-  validateForm()
-  {
-    if(!emailTextEditingController.text.contains("@"))
-    {
-      Fluttertoast.showToast(msg: "Email address is not Valid.");
-    }
-    else if(passwordTextEditingController.text.isEmpty)
-    {
-      Fluttertoast.showToast(msg: "Password is required.");
-    }
-    else
-    {
+  validateForm() {
+    if (!emailTextEditingController.text.contains("@")) {
+      Fluttertoast.showToast(msg: "Adresse email invalide.");
+    } else if (passwordTextEditingController.text.isEmpty) {
+      Fluttertoast.showToast(msg: "Mot de passe requis.");
+    } else {
       loginDriverNow();
     }
   }
 
-  loginDriverNow() async
-  {
+  loginDriverNow() async {
     showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext c)
-        {
-          return ProgressDialog(message: "Processing, Please wait...",);
-        }
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext c) {
+        return ProgressDialog(message: "Traitement en cours, veuillez patienter...");
+      },
     );
 
-    final User? firebaseUser = (
-        await fAuth.signInWithEmailAndPassword(
-          email: emailTextEditingController.text.trim(),
-          password: passwordTextEditingController.text.trim(),
-        ).catchError((msg){
-          Navigator.pop(context);
-          Fluttertoast.showToast(msg: "Error: " + msg.toString());
-        })
-    ).user;
+    final User? firebaseUser = (await fAuth
+        .signInWithEmailAndPassword(
+      email: emailTextEditingController.text.trim(),
+      password: passwordTextEditingController.text.trim(),
+    ).catchError((msg) {
+      Navigator.pop(context);
+      Fluttertoast.showToast(msg: "Erreur: " + msg.toString());
+    }))
+        .user;
 
-    if(firebaseUser != null)
-    {
-      DatabaseReference driversRef = FirebaseDatabase.instance.ref().child("drivers");
-      driversRef.child(firebaseUser.uid).once().then((driverKey)
-      {
+    if (firebaseUser != null) {
+      DatabaseReference driversRef =
+      FirebaseDatabase.instance.ref().child("drivers");
+      driversRef.child(firebaseUser.uid).once().then((driverKey) {
         final snap = driverKey.snapshot;
-        if(snap.value != null)
-        {
+        if (snap.value != null) {
           currentFirebaseUser = firebaseUser;
-          Fluttertoast.showToast(msg: "Login Successful.");
-          Navigator.push(context, MaterialPageRoute(builder: (c)=> const MySplashScreen()));
-        }
-        else
-        {
-          Fluttertoast.showToast(msg: "No record exist with this email.");
+          Fluttertoast.showToast(msg: "Connexion réussie.");
+          Navigator.push(
+              context, MaterialPageRoute(builder: (c) => const MySplashScreen()));
+        } else {
+          Fluttertoast.showToast(
+              msg: "Aucun enregistrement trouvé avec cet email.");
           fAuth.signOut();
-          Navigator.push(context, MaterialPageRoute(builder: (c)=> const MySplashScreen()));
+          Navigator.push(
+              context, MaterialPageRoute(builder: (c) => const MySplashScreen()));
         }
       });
-    }
-    else
-    {
+    } else {
       Navigator.pop(context);
-      Fluttertoast.showToast(msg: "Error Occurred during Login.");
+      Fluttertoast.showToast(msg: "Erreur lors de la connexion.");
     }
   }
 
@@ -97,16 +101,12 @@ class _LoginScreenState extends State<LoginScreen>
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-
-              const SizedBox(height: 30,),
-
+              const SizedBox(height: 30),
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Image.asset("images/logo1.png"),
               ),
-
-              const SizedBox(height: 10,),
-
+              const SizedBox(height: 10),
               const Text(
                 "Login as a Driver",
                 style: TextStyle(
@@ -115,13 +115,10 @@ class _LoginScreenState extends State<LoginScreen>
                   fontWeight: FontWeight.bold,
                 ),
               ),
-
               TextField(
                 controller: emailTextEditingController,
                 keyboardType: TextInputType.emailAddress,
-                style: const TextStyle(
-                    color: Colors.grey
-                ),
+                style: const TextStyle(color: Colors.grey),
                 decoration: const InputDecoration(
                   labelText: "Email",
                   hintText: "Email",
@@ -141,14 +138,11 @@ class _LoginScreenState extends State<LoginScreen>
                   ),
                 ),
               ),
-
               TextField(
                 controller: passwordTextEditingController,
                 keyboardType: TextInputType.text,
                 obscureText: true,
-                style: const TextStyle(
-                    color: Colors.grey
-                ),
+                style: const TextStyle(color: Colors.grey),
                 decoration: const InputDecoration(
                   labelText: "Password",
                   hintText: "Password",
@@ -168,13 +162,10 @@ class _LoginScreenState extends State<LoginScreen>
                   ),
                 ),
               ),
-
-              const SizedBox(height: 20,),
-
+              const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: ()
-                {
-                  validateForm();
+                onPressed: () {
+                  authenticateBiometric(); // Appeler l'authentification biométrique
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.lightGreenAccent,
@@ -187,18 +178,16 @@ class _LoginScreenState extends State<LoginScreen>
                   ),
                 ),
               ),
-
               TextButton(
                 child: const Text(
                   "Do not have an Account? SignUp Here",
                   style: TextStyle(color: Colors.grey),
                 ),
-                onPressed: ()
-                {
-                  Navigator.push(context, MaterialPageRoute(builder: (c)=> SignUpScreen()));
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (c) => SignUpScreen()));
                 },
               ),
-
             ],
           ),
         ),
